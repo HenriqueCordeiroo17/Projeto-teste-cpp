eab8f62f3fe7da073bbaa15a08358a10aaf14dc7
Henrique Cordeiro <Henriquedebritoocordeiro@gmail.com>
2026-03-01 05:59:53 -0300

    Adiciona verificador de numero par ou impar

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Projeto funcionando!" << std::endl;
+    int numero;
+
+    std::cout << "Digite um numero: ";
+    std::cin >> numero;
+
+    if (numero % 2 == 0) {
+        std::cout << "O numero eh par." << std::endl;
+    } else {
+        std::cout << "O numero eh impar." << std::endl;
+    }
+
     return 0;
 }
